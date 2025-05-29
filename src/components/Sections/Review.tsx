50f1172be24af1735c487df95a441c826c6398e8
Ihor Hromadskiy <gromadskiyigor@gmail.com>
2025-05-29 18:29:46 +0300

    [Update] Review styles

--- src/components/Sections/Review.tsx
+++ src/components/Sections/Review.tsx
@@ -1,100 +1,106 @@
 "use client";
 
 import { Button } from "@telegram-apps/telegram-ui";
-import { FaChevronRight } from "react-icons/fa";
 import { useState } from "react";
 import { useTranslations } from "next-intl";
 import Icon from "@/helpers/Icon";
-import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import uaFlag from "@/img/flags/ua.svg";
-import facebook from "@/img/social/facebook.svg";
-import { GoBell } from "react-icons/go";
-
 const Review = () => {
-  const [level, setLevel] = useState<string>("1");
-  const [status, setStatus] = useState<string>("“Нулевый”");
-  const [discount, setDiscount] = useState<string>("1");
-  const [nextLevel, setNextLevel] = useState<string>("154");
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-
   const t = useTranslations("Review");
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isContentVisible, setIsContentVisible] = useState(true);
+
+  const toggleContentVisibility = () => {
+    setIsContentVisible((prev) => !prev);
+  };
 
   return (
-    <div>
-      <div className="flex justify-between items-center pb-4 max-w-container mx-auto">
+    <div
+      className=" pb-2 mb-6"
+      style={{
+        borderBottom: "1px solid rgba(0, 0, 0, 0.10)",
+      }}
+    >
+      <div className="flex justify-between items-center pb-4 max-w-container mx-auto pl-3">
         <Button
-          className="text-[rgba(0,0,0,0.80)] border-[1px] border-solid border-[#0000000D]"
-          before={<Icon name="icon-hide" width={19.5} height={15} />}
+          className="text-[rgba(0,0,0,0.80)] border-[1px] border-solid border-[#0000000D] transition-all duration-300"
+          before={
+            <Icon
+              name={isContentVisible ? "icon-hide" : "icon-show"}
+              width={19.5}
+              height={15}
+            />
+          }
           mode="outline"
           size="s"
+          onClick={toggleContentVisibility}
         >
-          {t("hideBtn")}
+          {t(isContentVisible ? "hideBtn" : "showBtn")}
         </Button>
         <Button
-          className="bg-[rgba(0,122,255,0.03)]  text-accentText"
+          className="bg-[rgba(0,122,255,0.03)] text-accentText"
           mode="bezeled"
           size="s"
         >
           {t("viewAll")}
         </Button>
       </div>
-      <Swiper
-        spaceBetween={8}
-        slidesPerView="auto"
-        centeredSlides={true}
-        loop={true}
-        pagination={{
-          el: ".swiper-pagination",
-          clickable: true,
-          renderBullet: (index, className) => {
-            return `
-              <span class="${className} w-3 h-3 rounded-full transition ${
-              currentSlide === index
-                ? "bg-accentText w-4 h-4 mt-[48px]"
-                : "bg-gray-300 mt-[50px]"
-            }"></span>
-            `;
-          },
-        }}
-        onSlideChange={(swiper) => setCurrentSlide(swiper.activeIndex)}
-        modules={[Pagination]}
-        style={{
-          paddingBottom: "48px",
-          position: "relative",
-        }}
-      >
-        {Array.from({ length: 5 }).map((_, index) => (
-          <SwiperSlide key={index} style={{ width: "173px" }}>
-            <div
-              style={{
-                borderRadius: "16px",
-                background: "#B5B5B5",
-                width: "173px",
-                height: "198px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            ></div>
-          </SwiperSlide>
-        ))}
-        <div className="swiper-pagination custom-dots"></div>
-      </Swiper>
       <div
-        className="max-w-container mx-auto pb-6 mb-6"
-        style={{
-          borderBottom: "1px solid rgba(0, 0, 0, 0.10)",
-        }}
+        className={`transition-all duration-300 ease-in-out ${
+          isContentVisible ? "opacity-100" : "opacity-0 h-0 overflow-hidden"
+        }`}
       >
-        <Button className="w-[100%]" mode="filled" size="s">
-          {t("makeReview")}
-        </Button>
+        <Swiper
+          spaceBetween={8}
+          slidesPerView="auto"
+          centeredSlides={true}
+          loop={true}
+          pagination={{
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: (index, className) => {
+              return `
+                <span class="${className} w-3 h-3 rounded-full transition ${
+                currentSlide === index
+                  ? "bg-accentText w-4 h-4 mt-[48px]"
+                  : "bg-gray-300 mt-[50px]"
+              }"></span>
+              `;
+            },
+          }}
+          onSlideChange={(swiper) => setCurrentSlide(swiper.activeIndex)}
+          modules={[Pagination]}
+          style={{
+            paddingBottom: "48px",
+            position: "relative",
+          }}
+        >
+          {Array.from({ length: 5 }).map((_, index) => (
+            <SwiperSlide key={index} style={{ width: "173px" }}>
+              <div
+                style={{
+                  borderRadius: "16px",
+                  background: "#B5B5B5",
+                  width: "173px",
+                  height: "198px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+          <div className="swiper-pagination custom-dots"></div>
+        </Swiper>
+        <div className="max-w-container mx-auto pb-4">
+          <Button className="w-[100%]" mode="filled" size="s">
+            {t("makeReview")}
+          </Button>
+        </div>
       </div>
     </div>
   );
